6e1b0ad2cafdd92c6d5a955be5f934f9f072a949
Ted L <dstrike@gmail.com>
2023-12-05 18:16:34 -0500

    Speed

--- Advent-of-Code/2023/Day05.cs
+++ Advent-of-Code/2023/Day05.cs
@@ -6,23 +6,23 @@ namespace AdventOfCode._2023;
 
 public partial class Day05 : AdventBase
 {
-    private readonly List<List<Range>> _maps = [];
+    private readonly List<List<DestinationSource>> _maps = [];
 
-    private record struct Range(long Destination, long Source, long Count);
-    private record struct Result(long Location, long Consumed);
-    private record struct SeedRange(long Seed, long Range);
+    private record struct DestinationSource(long Destination, long Source, long Count);
+    private record struct Result(long Value, long FitThrough);
+    private record struct Range(long Value, long Count);
 
     protected override void InternalOnLoad()
     {
         for (var i = 1; i < 8; i++)
         {
-            var map = new List<Range>();
+            var map = new List<DestinationSource>();
             _maps.Add(map);
             var block = Input.Blocks[i];
             foreach (var line in block.Lines.Skip(1))
             {
                 var numbers = line.ParseLongs().ToArray();
-                map.Add(new Range
+                map.Add(new DestinationSource
                 {
                     Destination = numbers[0],
                     Source = numbers[1],
@@ -67,7 +67,7 @@ public partial class Day05 : AdventBase
     private Result EatSeeds(long seed)
     {
         var current = seed;
-        var consumed = long.MaxValue;
+        var fitThrough = long.MaxValue;
         foreach (var map in _maps)
         {
             long next = -1;
@@ -78,14 +78,14 @@ public partial class Day05 : AdventBase
                 var diff = current - range.Source;
                 next = range.Destination + diff;
                 var remaining = range.Source + range.Count - current;
-                if (consumed > remaining) consumed = remaining;
+                if (fitThrough > remaining) fitThrough = remaining;
                 break;
             }
 
             if (next >= 0) current = next;
         }
 
-        return new Result(current, consumed);
+        return new Result(current, fitThrough);
     }
 
     protected override object InternalPart2()
@@ -93,25 +93,79 @@ public partial class Day05 : AdventBase
         var seeds = Input.Blocks[0].Text
             .ParseLongs()
             .Chunk(2)
-            .Select(x => new SeedRange(x[0], x[1]))
-            .OrderBy(x => x.Seed)
+            .Select(x => new Range(x[0], x[1]))
+            .OrderBy(x => x.Value)
             .ToArray();
+
+        return LocationFirst(seeds);
 
         var seedLocations = new List<long>();
 
         long current = -1;
         foreach (var seedRange in seeds)
         {
-            for (var seed = seedRange.Seed; seed < seedRange.Seed + seedRange.Range; seed++)
+            for (var seed = seedRange.Value; seed < seedRange.Value + seedRange.Count; seed++)
             {
                 if (current > seed) continue;
                 var result = EatSeeds(seed);
-                current = seed + result.Consumed;
-                seedLocations.Add(result.Location);
-                if (current >= seedRange.Seed + seedRange.Range) break;
+                current = seed + result.FitThrough;
+                seedLocations.Add(result.Value);
+                if (current >= seedRange.Value + seedRange.Count) break;
             }
         }
 
         return seedLocations.Min();
+    }
+
+    private long LocationFirst(Range[] seeds)
+    {
+        long next = 0;
+        foreach (var range in _maps[6].OrderBy(x => x.Destination).Select(x => new Range(x.Destination, x.Count)))
+        {
+            while (next < range.Value + range.Count)
+            {
+                var location = next < range.Value ? range.Value : next;
+                var check = new Range(location, range.Count - (location - range.Value));
+                var result = SpitSeeds(check);
+                var resultMax = result.Value + result.FitThrough;
+                foreach (var seed in seeds)
+                {
+                    var seedMax = seed.Value + seed.Count;
+                    if (result.Value >= seed.Value && seed.Value <= resultMax
+                        && seedMax >= result.Value && seedMax >= result.Value)
+                    {
+                        return location;
+                    }
+                }
+                next = range.Value + result.FitThrough;
+            }
+        }
+
+        throw new Exception();
+    }
+
+    private Result SpitSeeds(Range locationRange)
+    {
+        var fitThrough = locationRange.Count;
+        var current = locationRange.Value;
+        for (var i = 6; i >= 0; i--)
+        {
+            var map = _maps[i];
+            long next = -1;
+            foreach (var range in map)
+            {
+                if (current < range.Destination || current >= range.Destination + range.Count) continue;
+
+                var diff = current - range.Destination;
+                next = range.Source + diff;
+                var remaining = range.Destination + range.Count - current;
+                if (fitThrough > remaining) fitThrough = remaining;
+                break;
+            }
+
+            if (next >= 0) current = next;
+        }
+
+        return new Result(current, fitThrough);
     }
 }
